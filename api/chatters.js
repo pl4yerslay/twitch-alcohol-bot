ff0424da62839a7824e4dba2a14ec39ec18fa423
pl4yerslay <pytfil07@gmail.com>
2026-08-13 03:07:37 +0200

    Update chatters.js

--- api/chatters.js
+++ api/chatters.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   return res.status(200).json({
-    client_id_exists: !!process.env.TWITCH_CLIENT_ID,
-    client_secret_exists: !!process.env.TWITCH_CLIENT_SECRET
+    node_env: process.env.NODE_ENV,
+    all_env_keys: Object.keys(process.env)
   });
 }
